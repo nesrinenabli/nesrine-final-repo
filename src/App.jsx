e7cace93e6a993866b19ad710414f2efecd0cc96
yasmine rekik <rekikyasmine@tbs.u-tunis.tn>
2025-04-15 14:47:32 +1300

    W5 Arrow and Handler Functions in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,40 @@
 import * as React from "react";
+const App = () => {
+  return (
+    <div>
+    <h1>My Hacker Stories</h1>
+    <Search />
+    <hr />
+    <List />
+    </div>
+    );
+}
+  const Search = ()  => {
+    const handleChange=(event) => {
+      console.log(event);
+      console.log(event.target.value);
+    };
+    return(
+    <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={handleChange} />
+    </div>
+    );
+  };
 
-function List() {
-  return ( 
-    <ul>  
-  {list.map(function (item) { 
-   return ( 
-   <li key={item.objectID}> 
-     <span>  
-   <a href={item.url}>{item.title}</a> 
-   </span> 
-       <span>{item.author}</span> 
-      <span>{item.num_comments}</span>    
-        <span>{item.points}</span> 
-        </li> 
-       ); 
-       })} 
-      </ul> 
-        ); 
-      } 
-    
-    function App()
-       { 
-          return (     
-          <div>       
-            <h1>My Hacker Stories</h1>
-         <label htmlFor="search">Search: </label>
-        <input id="search" type="text" />
-       <hr /> 
-          <List /> 
-            </div>
-         ); } 
-         
-    function Search() 
-         {   
-          return ( 
-            <div>       
-              <label htmlFor="search">Search: </label>       
-              <input id="search" type="text" />
-                   </div>   
-                   );
-                  
-                } 
-    function App() {
-                   return (
-                     <div>
-               <h1>My Hacker Stories</h1> 
-               <Search /> 
-               <hr /> 
-               <List />
-                </div>
-                 ); 
-                } 
+const List = () => (
+  <ul>
+  {list.map((item) => (
+  <li key={item.objectID}>
+  <span>
+  <a href={item.url}>{item.title}</a>
+  </span>
+  <span>{item.author}</span>
+  <span>{item.num_comments}</span>
+  <span>{item.points}</span>
+  </li>
+  ))}
+  </ul>
+  );
 
   export default App;
-
