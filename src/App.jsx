4ff2284b488c0509149df8daa9c38a524cbc9f4b
yasmine rekik <rekikyasmine@tbs.u-tunis.tn>
2025-04-22 14:54:22 +1300

    W6 Adding callback handler

--- src/App.jsx
+++ src/App.jsx
@@ -20,21 +20,29 @@ const App = () => {
       objectID: 1,
     },
   ];
+  //A
+  const handleSearch = (event) => {
+    //C
+    console.log(event.target.value);
+  };
   return (
     <div>
     <h1>My Hacker Stories</h1>
-    <Search />
+    {/* // B */}
+    <Search onSearch={handleSearch} />
     <hr />
     <List list={stories} />
     </div>
     );
  };
-const Search = ()  => {
+const Search = (props)  => {
   console.log('Search renders'); 
 
    const [searchTerm, setSearchTerm] = React.useState('');
    const handleChange=(event) => {
      setSearchTerm(event.target.value);
+     //B
+     props.onnSearch(event);
    };
       
     return(
