9fa4df47549ab2227403a95267541a5860d71872
yasmine rekik <rekikyasmine@tbs.u-tunis.tn>
2025-04-22 15:21:49 +1300

    W6 Filtering the list

--- src/App.jsx
+++ src/App.jsx
@@ -20,35 +20,39 @@ const App = () => {
       objectID: 1,
     },
   ];
-  //A
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   const handleSearch = (event) => {
-    //C
-    console.log(event.target.value);
+  
+    setSearchTerm(event.target.value);
   };
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  
+);
   return (
     <div>
     <h1>My Hacker Stories</h1>
-    {/* // B */}
     <Search onSearch={handleSearch} />
     <hr />
-    <List list={stories} />
+    <List list={searchedStories} />
     </div>
     );
- };
+  };
 const Search = (props)  => {
   console.log('Search renders'); 
 
    const [searchTerm, setSearchTerm] = React.useState('');
    const handleChange=(event) => {
      setSearchTerm(event.target.value);
-     //B
-     props.onnSearch(event);
+  
+     props.onSearch(event);
    };
       
     return(
      <div>
        <label htmlFor="search">Search: </label>
-       <input id="search" type="text" onChange={handleChange} />
+       <input id="search" type="text" onChange={props.onSearch} />
         <p>
             Searching for <strong> {searchTerm}</strong>.
         </p>
